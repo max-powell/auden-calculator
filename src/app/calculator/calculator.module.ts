d18014a1e8975c236461e838b68a7abd13e3b4ed
Max Powell <maxfpowell@gmail.com>
2022-10-17 13:48:17 +0100

    Built for test display

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { NumberpadComponent } from './components/numberpad/numberpad.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
-  declarations: [CalculatorComponent, NumberpadComponent],
+  declarations: [CalculatorComponent, NumberpadComponent, DisplayComponent],
   imports: [CommonModule],
   exports: [CalculatorComponent],
 })
