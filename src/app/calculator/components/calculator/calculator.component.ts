aac9a3d112ead1354465b69cf1d6c27232b5209d
Max Powell <maxfpowell@gmail.com>
2022-10-17 14:15:34 +0100

    Connected numberpad to calculator and calculator service

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,24 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CalculatorService } from 'src/app/shared/services/calculator.service';
-import {
-  NumberpadButtonConfig,
-  NumberpadButtonModifier,
-  Operator,
-} from '../../models';
+import { Operator } from '../../models';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss'],
 })
-export class CalculatorComponent implements OnInit {
-  public displayValue: string = '0';
+export class CalculatorComponent {
+  public calculationString = '0';
 
   constructor(private calculatorService: CalculatorService) {}
 
-  public ngOnInit(): void {
-    this.displayValue = this.calculatorService
-      .calculate(Operator.Add, 20, 20)
-      .toString();
+  public handleButtonPress(value: string): void {
+    switch (value) {
+      case 'reset':
+        this.resetCalculation();
+        break;
+      case Operator.Equals:
+        this.calculate();
+        break;
+      default:
+        this.addToCalculationString(value);
+        break;
+    }
+  }
+
+  private resetCalculation(): void {
+    this.calculationString = '0';
+  }
+
+  private calculate(): void {
+    const [operand1, operator, operand2] = this.calculationString.split(' ');
+    const calculatedValue = this.calculatorService.calculate(
+      operator as Operator,
+      parseInt(operand1),
+      parseInt(operand2)
+    );
+    this.calculationString = calculatedValue.toString();
+  }
+
+  private addToCalculationString(value: string): void {
+    if (this.calculationString === '0') {
+      this.calculationString = value;
+    } else {
+      this.calculationString += value;
+    }
   }
 }
--- src/app/calculator/components/numberpad/numberpad.component.ts
+++ src/app/calculator/components/numberpad/numberpad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {
   NumberpadButtonConfig,
   NumberpadButtonModifier,
@@ -11,22 +11,36 @@ import {
   styleUrls: ['./numberpad.component.scss'],
 })
 export class NumberpadComponent {
+  @Output() buttonPress = new EventEmitter<string>();
+
   public numberpadConfig: NumberpadButtonConfig[] = [
     { text: 'C', value: 'reset', wide: true },
-    { text: Operator.Divide, value: Operator.Divide, operator: true },
+    { text: Operator.Divide, value: ` ${Operator.Divide} `, operator: true },
     { text: '7', value: '7' },
     { text: '8', value: '8' },
     { text: '9', value: '9' },
-    { text: Operator.Multiply, value: Operator.Multiply, operator: true },
+    {
+      text: Operator.Multiply,
+      value: ` ${Operator.Multiply} `,
+      operator: true,
+    },
     { text: '4', value: '4' },
     { text: '5', value: '5' },
     { text: '6', value: '6' },
-    { text: Operator.Subtract, value: Operator.Subtract, operator: true },
+    {
+      text: Operator.Subtract,
+      value: ` ${Operator.Subtract} `,
+      operator: true,
+    },
     { text: '1', value: '1' },
     { text: '2', value: '2' },
     { text: '3', value: '3' },
-    { text: Operator.Add, value: Operator.Add, operator: true },
+    { text: Operator.Add, value: ` ${Operator.Add} `, operator: true },
     { text: '0', value: '0', wide: true },
     { text: Operator.Equals, value: Operator.Equals },
   ];
+
+  public handleButtonPress(value: string): void {
+    this.buttonPress.emit(value);
+  }
 }
