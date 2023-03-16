7ea89abdd03a2a7777e03d31700fb4dfc8601e51
Max Powell <maxfpowell@gmail.com>
2023-03-16 10:23:06 +0000

    Tidied scss files

--- src/app/calculator/components/display/display.component.ts
+++ src/app/calculator/components/display/display.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-display',
-  templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss'],
+  templateUrl: './display.component.html'
 })
 export class DisplayComponent {
   @Input() display: string = 'test';
